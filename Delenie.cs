65742e62617eeaf0948886fc678d87228feafb46
Velcheor137 <84522679+Velcheor137@users.noreply.github.com>
2021-06-24 16:14:29 +0500

    добавили проверку деления на ноль

--- Delenie.cs
+++ Delenie.cs
@@ -16,9 +16,13 @@ namespace Calc
         {
             if (st1.Length <= 15 && st2.Length <= 15)
             {
+                string result2;
                 double s1_1 = double.Parse(st1);
                 double s1_2 = double.Parse(st2);
-                string result2 = (s1_1 / s1_2) + "";
+                if (s1_2 != 0)
+                    result2 = (s1_1 / s1_2) + "";
+                else
+                    result2 = "Ошибка! Вы поделили на ноль!";
                 return result2;
             }
             else
@@ -26,10 +30,13 @@ namespace Calc
                 ExtendedNumerics.BigRational s1 = ExtendedNumerics.BigRational.Parse(st1);
                 ExtendedNumerics.BigRational s2 = ExtendedNumerics.BigRational.Parse(st2);
                 string result;
-                ExtendedNumerics.BigRational u;
-                u = s1 / s2;
-                result = u + "";
-
+                ExtendedNumerics.BigRational u, o = ExtendedNumerics.BigRational.Parse("0");
+                if (s2 != o)
+                {
+                    u = s1 / s2;
+                    result = u + "";
+                }
+                else result = "Ошибка! Вы поделили на 0!";
                 return result;
             }
         }
